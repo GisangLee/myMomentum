0f584629114002a391be42fafa13d5f579c29ffd
GisangLee <masterkorea01@naver.com>
2020-06-04 11:46:04 +0900

    in progress with todos

--- todos.js
+++ todos.js
@@ -3,58 +3,81 @@ const toDoInput = toDoForm.querySelector(".toDoInput");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
+const DISAPPEAR = "disappear";
 
-const toDos = [];
+let toDos = [];
 
-function saveToDos(toDos) {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+function saveToDos(text) {
+  localStorage.setItem(TODOS_LS, JSON.stringify(text));
 }
 
-function paintToDo(text) {
+function delToDos(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  toDoForm.classList.add(SHOW_CL);
+  saveToDos(toDos);
+}
+
+function paintToDos(text) {
   const li = document.createElement("li");
   const delbtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
 
-  delbtn.innerHTML = "❌";
+  delbtn.innerHTML = "💥";
+  delbtn.addEventListener("click", delToDos);
+  toDoForm.classList.remove(SHOW_CL);
   span.innerText = text;
-  li.appendChild(delbtn);
+
   li.appendChild(span);
+  li.appendChild(delbtn);
   li.id = newId;
+  li.classList.add("jstodo");
   toDoList.appendChild(li);
 
   const toDoObj = {
-    text: text,
     id: newId,
+    text: text,
   };
   toDos.push(toDoObj);
   saveToDos(toDos);
 }
 
-function handleSubmit(event) {
+function handleSubmitToDos(event) {
   event.preventDefault();
-  const currentValue = toDoInput.value;
-  paintToDo(currentValue);
+  const toDoValue = toDoInput.value;
+  paintToDos(toDoValue);
   toDoInput.value = "";
 }
 
+function askForToDos() {
+  toDoForm.classList.add(SHOW_CL);
+  toDoForm.addEventListener("submit", handleSubmitToDos);
+}
+
 function loadToDos() {
-  const currentuser = localStorage.getItem(USER_LS);
   const loadedToDos = localStorage.getItem(TODOS_LS);
-  if (currentuser !== null) {
+  const user = localStorage.getItem(USER_LS);
+  if (user !== null) {
     if (loadedToDos !== null) {
       const parsedToDos = JSON.parse(loadedToDos);
-      parsedToDos.forEach(function (todo) {
-        paintToDo(todo.text);
+      parsedToDos.forEach(function (toDo) {
+        paintToDos(toDo.text);
       });
     } else {
+      askForToDos();
     }
   }
 }
 
 function init() {
   loadToDos();
-  toDoForm.addEventListener("submit", handleSubmit);
+  toDoForm.addEventListener("submit", handleSubmitToDos);
 }
 
 init();
